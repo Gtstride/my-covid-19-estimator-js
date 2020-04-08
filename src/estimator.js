07ee83f9e7ba546188a1db9b9e92764655ad02df
Godstime <lewisagho47@gmail.com>
2020-04-08 07:28:49 +0100

    Fix chalenge 1 error

--- src/estimator.js
+++ src/estimator.js
@@ -1,51 +1,13 @@
-const IMPACTLEVEL = {
-  NORMAL: 1,
-  SEVERE: 2
-};
-
-const calculateCurrentlyInfected = (reportedCases, impactLevel) => {
-  if (impactLevel === IMPACTLEVEL.NORMAL) {
-    return reportedCases * 10;
-  }
-
-  if (impactLevel === IMPACTLEVEL.SEVERE) {
-    return reportedCases * 50;
-  }
-
-  return reportedCases * 10;
-};
-
-const calculateinfectionsByRequestedTime = (currentlyInfected, factor) => {
-  const infectionQuotient = 2 ** factor;
-  return currentlyInfected * infectionQuotient;
-};
-
 const covid19ImpactEstimator = (data) => {
-  const input = data;
+  const { reportedCases } = data;
   const impact = {};
   const severeImpact = {};
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 10;
+  severeImpact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 10;
 
-  impact.currentlyInfected = calculateCurrentlyInfected(input.reportedCases, 1);
-  severeImpact.currentlyInfected = calculateCurrentlyInfected(
-    input.reportedCases,
-    2
-  );
-
-  impact.infectionsByRequestedTime = calculateinfectionsByRequestedTime(
-    impact.currentlyInfected,
-    9
-  );
-
-  severeImpact.infectionsByRequestedTime = calculateinfectionsByRequestedTime(
-    severeImpact.currentlyInfected,
-    9
-  );
-
-  return {
-    data: input,
-    impact,
-    severeImpact
-  };
+  return { data, impact, severeImpact };
 };
 
 export default covid19ImpactEstimator;
