5a1a089a66204e8bb796ab96164782c96a6cb4f7
Godstime <lewisagho47@gmail.com>
2020-04-08 07:58:37 +0100

    First assessment

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,20 @@
-const covid19ImpactEstimator = (data) => {
-  const { reportedCases } = data;
-  const impact = {};
-  const severeImpact = {};
-  impact.currentlyInfected = reportedCases * 10;
-  severeImpact.currentlyInfected = reportedCases * 50;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 10;
-  severeImpact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 10;
-
-  return { data, impact, severeImpact };
-};
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: this.currentlyInfected * 512,
+    severeCasesByRequestedTime: (15 / 100) * this.infectionsByRequestedTime,
+    hospitalBedsByRequestedTime:
+      (35 / 100) * data.totalHospitalBeds - this.severeCasesByRequestedTime
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: this.currentlyInfected * 512,
+    severeCasesByRequestedTime:
+      (15 / 100) * this.infectionsByRequestedTime.covid19ImpactEstimator,
+    hospitalBedsByRequestedTime:
+      (35 / 100) * data.totalHospitalBeds - this.severeCasesByRequestedTime
+  }
+});
 
 export default covid19ImpactEstimator;
