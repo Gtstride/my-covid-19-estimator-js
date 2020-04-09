f0a9f51feeb49d621120758363ca32c594af5883
Godstime <lewisagho47@gmail.com>
2020-04-09 22:00:08 +0100

    Challenge two worked on

--- src/estimator.js
+++ src/estimator.js
@@ -11,13 +11,13 @@ const calculatePeriod = (data) => {
 
 const getDays = (data) => {
   if (data.periodType === 'months') {
-    return 30 * data.timeToElapse;
+    return 30 * Math.floor(data.timeToElapse);
   }
   if (data.periodType === 'weeks') {
-    return 7 * data.timeToElapse;
+    return 7 * Math.floor(data.timeToElapse);
   }
 
-  return 1 * data.timeToElapse;
+  return 1 * Math.floor(data.timeToElapse);
 };
 
 const currentInfections = (data) => {
