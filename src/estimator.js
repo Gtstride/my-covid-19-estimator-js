4b136d19c257153bb66a53d3207ce3e448660f55
Godstime <lewisagho47@gmail.com>
2020-04-10 01:28:53 +0100

    Challenge two change Math.floor to math.trun()

--- src/estimator.js
+++ src/estimator.js
@@ -48,7 +48,7 @@ const projectedInfections = (data) => {
   const infectionsByRequestedTime = currentlyInfected * calculatePeriod(data);
   const severeCasesByRequestedTime = (15 / 100) * infectionsByRequestedTime;
   const compute = (35 / 100) * data.totalHospitalBeds;
-  const hospitalBedsByRequestedTime = Math.floor(
+  const hospitalBedsByRequestedTime = Math.trunc(
     compute - severeCasesByRequestedTime
   );
   const casesForICUByRequestedTime = (5 / 100) * infectionsByRequestedTime;
