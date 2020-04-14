814a3dcd9a98f0b81cf58e437323bce75e7e0815
Godstime <lewisagho47@gmail.com>
2020-04-14 21:31:38 +0100

    ignoring fil

--- src/controller.js
+++ src/controller.js
@@ -1,77 +1,77 @@
-// const covid19ImpactEstimator = require('./estimator').data;
+const covid19ImpactEstimator = require('./estimator').data;
 
-// // const impact = require('./estimator').data;
+// const impact = require('./estimator').data;
 
-// exports.dataPost = (req, res, next) => {
-//   const {
-//     name,
-//     averageAge,
-//     averageDailyIncomeInUSD,
-//     factor,
-//     periodType,
-//     timeToElapse,
-//     reportedCases,
-//     population,
-//     totalHospitalBeds
-//   } = req.body;
+exports.dataPost = (req, res, next) => {
+  const {
+    name,
+    averageAge,
+    averageDailyIncomeInUSD,
+    factor,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  } = req.body;
 
 
-//   // console.log(name, averageAge, averageDailyIncomeInUSD);
+  // console.log(name, averageAge, averageDailyIncomeInUSD);
 
-//   res.status(201).json({
-//     message: 'The Data Suuccessfully created',
-//     data: {
-//       id: new Date().toISOString(),
-//       name,
-//       averageAge,
-//       averageDailyIncomeInUSD,
-//       factor,
-//       periodType,
-//       timeToElapse,
-//       reportedCases,
-//       population,
-//       totalHospitalBeds
-//     }
-//   });
-//   next();
-// };
+  res.status(201).json({
+    message: 'The Data Suuccessfully created',
+    data: {
+      id: new Date().toISOString(),
+      name,
+      averageAge,
+      averageDailyIncomeInUSD,
+      factor,
+      periodType,
+      timeToElapse,
+      reportedCases,
+      population,
+      totalHospitalBeds
+    }
+  });
+  next();
+};
 
-// exports.getData = (req, res) => {
-//   const {
-//     currentlyInfected,
-//     infectionsByRequestedTime,
-//     severeCasesByRequestedTime,
-//     hospitalBedsByRequestedTime,
-//     casesForICUByRequestedTime,
-//     casesForVentilatorsByRequestedTime,
-//     dollarsInFlight
-//   } = req.body;
+exports.getData = (req, res) => {
+  const {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  } = req.body;
 
-//   return res.status(200).json({
-//     impact: // JSON.stringify([
-//       {
-//         currentlyInfected,
-//         infectionsByRequestedTime,
-//         severeCasesByRequestedTime,
-//         hospitalBedsByRequestedTime,
-//         casesForICUByRequestedTime,
-//         casesForVentilatorsByRequestedTime,
-//         dollarsInFlight
-//       },
-//     // ]),
+  return res.status(200).json({
+    impact: // JSON.stringify([
+      {
+        currentlyInfected,
+        infectionsByRequestedTime,
+        severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime,
+        dollarsInFlight
+      },
+    // ]),
 
-//     severeImpact: // JSON.stringify([
-//       {
-//         currentlyInfected,
-//         infectionsByRequestedTime,
-//         severeCasesByRequestedTime,
-//         hospitalBedsByRequestedTime,
-//         casesForICUByRequestedTime,
-//         casesForVentilatorsByRequestedTime,
-//         dollarsInFlight
-//       }
-//     // ])
-//   });
-// };
+    severeImpact: // JSON.stringify([
+      {
+        currentlyInfected,
+        infectionsByRequestedTime,
+        severeCasesByRequestedTime,
+        hospitalBedsByRequestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorsByRequestedTime,
+        dollarsInFlight
+      }
+    // ])
+  });
+};
 
-// module.export = covid19ImpactEstimator;
+module.export = covid19ImpactEstimator;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const controller = require('./controller');
+const express = require('express');
+const controller = require('./controller');
 
-// const router = express.Router();
+const router = express.Router();
 
-// // router.get('/sign-up', (req, res) => {
-// //   res.send('Going to get them');
-// // });
+// router.get('/sign-up', (req, res) => {
+//   res.send('Going to get them');
+// });
 
-// router.post('/on-covid-19/', controller.dataPost);
-// router.get('/on-covid-19/', controller.getData);
+router.post('/on-covid-19/', controller.dataPost);
+router.get('/on-covid-19/', controller.getData);
 
-// module.exports = router;
+module.exports = router;
